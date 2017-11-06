c8d66691caeb428dc097aead191d491d65175d55
Bongsu Kang <kbsbroad@gmail.com>
2017-11-07 01:04:23 +0900

    issue row에 clear 적용

--- config/app.config.js
+++ config/app.config.js
@@ -2,6 +2,7 @@ module.exports = {
   github: {
     // 이 앱을 이용하기 위해서는 github personal private key가 필요.
     // https://github.com/settings/tokens 여기서 새 키를 발급받아서 복사해서 붙여넣기.
-    accessToken: '5cecc0ea842036ac76d9c76973713fe6cb591a94'
+    // 일정시간이 지나면 파기되는 것 같기 때문에 테스트마다 발급해야함.
+    accessToken: '[put your personal private key]'
   }
 }
